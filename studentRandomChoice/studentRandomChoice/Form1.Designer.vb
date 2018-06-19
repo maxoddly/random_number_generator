b7e94a15b45145bc88262962d3c9f1d558ffc727
unknown <>
2018-06-19 23:23:25 +0800

    [init finish]

---
+++ studentRandomChoice/studentRandomChoice/Form1.Designer.vb
@@ -0,0 +1,71 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。  
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Font = New System.Drawing.Font("宋体", 200.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Label1.Location = New System.Drawing.Point(-1, -3)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(777, 472)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "1"
+        Me.Label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("宋体", 50.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(134, Byte))
+        Me.Button1.Location = New System.Drawing.Point(12, 472)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(764, 150)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "开始"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Timer1
+        '
+        '
+        'Form
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 18.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(784, 634)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form"
+        Me.Text = "学生点名系统"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ studentRandomChoice/studentRandomChoice/Form1.vb
@@ -0,0 +1,34 @@
+﻿Public Class Form
+    Dim allStuNum As Integer = 42
+    Dim allStuName As New ArrayList
+    Dim useName As Boolean
+    Private Sub Form_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Enabled = False
+        Timer1.Interval = 20
+        Try
+            Dim fn = FreeFile()
+            FileOpen(fn, "config.txt", OpenMode.Input)
+            Dim num As Integer
+            Input(fn, num)
+            allStuNum = num
+        Catch
+            allStuNum = 42
+        End Try
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Randomize()
+        Dim randNum As Integer = Rnd() * (allStuNum - 1) + 1
+        Label1.Text = randNum
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Button1.Text = "开始" Then
+            Timer1.Enabled = True
+            Button1.Text = "关闭"
+        Else
+            Timer1.Enabled = False
+            Button1.Text = "开始"
+        End If
+    End Sub
+End Class
